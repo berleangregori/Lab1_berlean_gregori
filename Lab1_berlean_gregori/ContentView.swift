02213826e70e83c5b77d6eebd561699ac259d9e7
Berlean Gregori <berlean.gregori@hotmail.com>
2025-02-12 22:54:47 -0500

    added necessary variables

--- Lab1_berlean_gregori/ContentView.swift
+++ Lab1_berlean_gregori/ContentView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var number: Int = Int.random(in: 1...100)
+    @State private var isCorrect: Bool? = nil
+    @State private var correctCount: Int = 0
+    @State private var wrongCount: Int = 0
+    @State private var attempts: Int = 0
+    @State private var timer: Timer? = nil
+    
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
